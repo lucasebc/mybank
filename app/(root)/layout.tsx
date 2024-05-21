59635b845e82d6b5d43979af1d498ac8fcc847b0
Lucas Emanuel <8696792+lucasebc@users.noreply.github.com>
2024-05-21 18:32:46 -0300

    implements sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,24 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = {firstName: 'Lucas', lastName: 'Emanuel' }
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={user} /> 
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src='/icons/logo.svg' height={30} width={30} alt="my bank logo" ></Image>
+            <div><MobileNav user={user}></MobileNav></div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
