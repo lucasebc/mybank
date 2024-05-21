6d53210dc1c97d7998e4f3ce1d0ea1932faf0a1c
Lucas Emanuel <8696792+lucasebc@users.noreply.github.com>
2024-05-21 19:39:00 -0300

    implement right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import HeaderComponent from '@/components/HeaderComponent'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceComponent from '@/components/TotalBalanceComponent'
 import React from 'react'
 
 const Home = () => {
+  const user = {firstName:'Lucas', lastName: 'Emanuel', email: 'lucasebc@outlook.com'}
+
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderComponent type='greeting' title='Welcome' user={user?.firstName || 'guest'} subtext='Access and manage your accounts' />
+          <TotalBalanceComponent accounts={[]} totalBanks={1} totalCurrentBalance={5000.35}></TotalBalanceComponent>
+        </header>
+
+        {/* recent transactions */}
+      </div>
+
+      <RightSidebar user={user} transactions={[]} banks={[{},{}]}></RightSidebar>
+    </section>
   )
 }
 
